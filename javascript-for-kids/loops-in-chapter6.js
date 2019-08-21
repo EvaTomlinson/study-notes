cdd4030c19553bfffa105431d6e39d02333a296a
Kahee Tomlinson <kkheva0215@gmail.com>
2019-08-21 19:43:16 -0400

    Create loops-in-chapter6.js

---
+++ javascript-for-kids/loops-in-chapter6.js
@@ -0,0 +1,27 @@
+// let i = 3;
+// while (i < 1000) {
+//   console.log(i);
+//   i = i * 3
+// }
+//------------------------------------------------------------------------------------
+
+// for (let i = 2; i < 1000; i = i * 3){
+//   console.log(i);
+// }
+//------------------------------------------------------------------------------------
+// const name = "Kardasian";
+// console.log("Hi" + " " + name + " " + "nice to meet you!");
+// if (name.lengh < 5) {
+//   console.log("Your name" + " " + name + " " +"is not too hard to memorize!")
+// } else {
+//   console.log("I think you should change your name to something short.")
+// }
+//How can I give spaces between Hi and name and nice to meet you!?
+// I can simply add "" between strings.
+
+//------------------------------------------------------------------------------------
+
+const english = ["grammar", "pronounciation", "writing", "slangs"];
+for (let i = 0; i < english.length; i++) {
+console.log("I wnat to learn" + " " + english[i] + " " + "really well" + " " + "so that Kurt gives me a lot of kisses!");
+}
