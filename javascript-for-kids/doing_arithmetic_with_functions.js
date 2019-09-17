dd6f311165562cdeeae26f2adee5c59eadc723f7
Kahee Tomlinson <kkheva0215@gmail.com>
2019-09-17 09:00:21 -0400

    created some examples from books

--- javascript-for-kids/doing_arithmetic_with_functions.js
+++ javascript-for-kids/doing_arithmetic_with_functions.js
@@ -1,8 +1,19 @@
-function add (a, b) {
-  return a + b;
-};
-function multiply (a, b) {
-  return a * b;
-};
-add(multiply(36325, 9824), 777);
+// function add (a, b) {
+//   return a + b;
+// };
+// function multiply (a, b) {
+//   return a * b;
+// };
+// add(multiply(36325, 9824), 777);
+
+/////////////////////////////////////////////
+
+function me (name, age) {
+  return name + age;  
+}
+function you (name, age) {
+  return name + age;
+}
+me("eva", 29);
+you("kurt", 31);
 
