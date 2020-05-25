358e202d252ebae26f57340e02bcb6559c7e6e22
Eva Tomlinson <kkheva0215@gmail.com>
2020-05-25 13:45:27 -0400

    finding the largest prime factor of a number

--- project-euler/problem3.ts
+++ project-euler/problem3.ts
@@ -1,12 +1,51 @@
 // How to run:
-// yarn ts-node project-euler/problem1.ts
+// yarn ts-node project-euler/problem3.ts
 
 // The prime factors of 13195 are 5, 7, 13 and 29.
 
 // What is the largest prime factor of the number 600851475143 ?
 
 function largestPrimeFactor(n) {
-  return 0;
+  let divisor = 2;
+  let lpf = 0;
+  while (divisor < n) {
+    const quotient = n / divisor;
+    const remainder = n % divisor;
+    if (remainder !== 0) {
+      // divisor is not a factor of n
+      // quotient is not an integer
+      divisor++;
+    } else {
+      // divisor is a factor of n
+      // quotient is a factor of n
+      n = n / divisor;
+      if (isPrime(divisor)) {
+        // divisor is a prime factor of n
+        if (divisor > lpf) {
+          // updating largest prime factor from divisor
+          lpf = divisor;
+        }
+      }
+      if (isPrime(quotient)) {
+        // quotient is a prime factor of n
+        if (quotient > lpf) {
+          // updating largest prime factor from quotient
+          lpf = quotient;
+          break;
+        }
+      }
+    }
+  }
+
+  return lpf;
+}
+function isPrime(n) {
+  for (let i = 2; i < n - 1; i++) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+  return true;
 }
 const exampleInput = 13195;
 const expectedExampleOutput = 29;
