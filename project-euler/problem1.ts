8c8a69d9aadb157a671838f5434c881c8dcc70e1
Eva Tomlinson <kkheva0215@gmail.com>
2020-05-25 11:35:57 -0400

    install ts with yarn

--- project-euler/problem1.ts
+++ project-euler/problem1.ts
@@ -1,5 +1,5 @@
 // How to run:
-// npx ts-node project-euler/problem1.ts
+// yarn ts-node project-euler/problem1.ts
 
 // If we list all the natural numbers below 10 that are multiples of 3 or 5,
 // we get 3, 5, 6 and 9. The sum of these multiples is 23.
