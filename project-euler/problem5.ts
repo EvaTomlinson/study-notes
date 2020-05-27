47308c8d5e5c1c51a4bf6e5e7cb58508a16f26d3
Eva Tomlinson <kkheva0215@gmail.com>
2020-05-27 14:18:40 -0400

    Create problem5.ts

---
+++ project-euler/problem5.ts
@@ -0,0 +1,41 @@
+// How to run:
+// yarn ts-node project-euler/problem5.ts
+
+// 2520 is the smallest number that can be divided by each of the numbers from
+// 1 to 10 without any remainder.
+
+// What is the smallest positive number that is evenly divisible by all of the
+// numbers from 1 to 20?
+
+function smallestNumberDivisibleByUpToN(maximumDivisor: number) {
+  return 0;
+}
+const testMaximumDivisor = 10;
+const testDividend = 2520;
+const expectedResult = 2520;
+if (smallestNumberDivisibleByUpToN(testMaximumDivisor) == expectedResult) {
+  console.log("You got it!");
+  console.log(
+    "smallest number divisible by 1 to 20",
+    smallestNumberDivisibleByUpToN(20)
+  );
+} else {
+  console.log("Try it again.");
+}
+
+function isDivisibleByNumbers(maximumDivisor: number, dividend: number) {
+  return true;
+}
+
+if (isDivisibleByNumbers(testMaximumDivisor, testDividend) == true) {
+  console.log("It works!");
+} else {
+  console.log("It failed.");
+}
+
+const nonDivisibleDividend = 2522;
+if (isDivisibleByNumbers(testMaximumDivisor, nonDivisibleDividend) == false) {
+  console.log("It works!");
+} else {
+  console.log("It failed.");
+}
