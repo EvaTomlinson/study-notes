6cccd19eb288c9503cf3bfa0431ac46ca6b6f256
Eva Tomlinson <kkheva0215@gmail.com>
2020-05-29 20:08:04 -0400

    Update problem7.ts

--- project-euler/problem7.ts
+++ project-euler/problem7.ts
@@ -5,6 +5,19 @@
 // we can see that the 6th prime is 13.
 
 // What is the 10 001st prime number?
+function nthPrimeNumber(n: number) {
+  return 0;
+}
+function testNthPrimeNumber() {
+  const exampleN = 6;
+  if (nthPrimeNumber(exampleN) == 13) {
+    console.log("Working");
+  } else {
+    console.log("Failed.");
+  }
+}
+testNthPrimeNumber();
+
 function isPrime(n: number) {
   return true;
 }
