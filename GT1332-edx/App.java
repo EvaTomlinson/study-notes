f1fc08f874e0890e606cebca7fc1e0e316706ea5
Kahee Tomlinson <48109534+EvaTomlinson@users.noreply.github.com>
2022-11-27 18:59:32 -0500

    added App and Rectangle

---
+++ GT1332-edx/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle(50, 100);
+        System.out.println(rectangle);
+    }  
+}
---
+++ GT1332-edx/Rectangle.java
@@ -0,0 +1,8 @@
+// module0 about constructor.
+public class Rectangle {
+    private double length, width;
+    public Rectangle(double l, double width) {
+        length = l;
+        width = width;
+    }
+}
